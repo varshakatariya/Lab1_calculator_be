caa0eb07f0cf363d309186f608d24726b912c36b
Varsha Amrutlal Katariya 012468079 <varshaamrutlal.katariya@sjsu.edu>
2018-03-03 02:36:51 -0800

    Calculator BE changes for routing operations

--- routes/calculatorFunctions.js
+++ routes/calculatorFunctions.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 //var fs = require('fs');
+var path = require('path');
 var number1 = './routes/';
 var number2 = './routes/';
+var operator = './routes/';
 var express = require('express');
 var bodyparser = require('body-parser');
 var app = express();
@@ -10,56 +12,37 @@ var app = express();
 var cors = require('cors');
 app.use(cors());
 app.use(bodyparser.json());
-app.get('/add', function(req,res)
-{
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
     console.log("inside add")
     var response = {result:""};
 
     number1 = Number(req.param('number1'));
     number2 = Number(req.param('number2'));
+    operator = req.param('operator');
 
-    response.result = (number1 + number2).toString();
+    console.log("Inside node JS :: operator " + operator);
+    console.log("Inside node JS Number 1 :: n1 " + number1);
+    console.log("Inside node JS Number 2:: n2 " + number2);
 
+    if(operator == "add"){
+        response.result = (number1 + number2).toString();
+    }else if(operator == "sub"){
+        response.result = (number1 - number2).toString();
+    }else if(operator == "mult"){
+        response.result = (number1 * number2).toString();
+    }else if(operator == "div"){
+        response.result = (number1 / number2).toString();
+    }
     res.send(response);
 
 });
-console.log("inside Rest");
-app.get('/sub', function(req,res)
-{
-    var response = "";
-console.log("inside sub")
-    number1 = Number(req.param('number1'));
-    number2 = Number(req.param('number2'));
 
-    response = (number1 - number2).toString();
-    res.send(response);
-
-});
-/*app.get('/mult', function(req,res)
-{
-    var response = "";
-
-    number1 = Number(req.param('number1'));
-    number2 = Number(req.param('number2'));
-
-    response = (number1 * number2).toString();
-    res.send(response);
-
-});
-app.get('/div', function(req,res)
-{
-    var response = "";
-
-    number1 = Number(req.param('number1'));
-    number2 = Number(req.param('number2'));
-
-    response = (number1 / number2).toString();
-    res.send(response);
-
-});*/
 app.listen(3001, function () {
     console.log('CORS-enabled web server listening on port 3001')
 })
 
-
-module.exports = router;
+module.exports = app;
